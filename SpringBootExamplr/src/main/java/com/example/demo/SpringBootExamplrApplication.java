f1aac4488a4dd3ac90a6d546377537e41ef267fc
nitinmore <nitinmore111@gmail.com>
2022-05-25 14:00:30 +0530

    Trying to commit

---
+++ SpringBootExamplr/src/main/java/com/example/demo/SpringBootExamplrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExamplrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExamplrApplication.class, args);
+	}
+
+}
